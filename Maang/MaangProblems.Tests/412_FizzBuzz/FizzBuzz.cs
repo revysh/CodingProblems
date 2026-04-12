7d34ec73ddd053b25975969ec390c8c4673cef0e
Ravishankar Rajappa <rrajappa@explorance.com>
2026-04-12 18:58:47 +0400

    Added Fizzbuz with solution

---
+++ Maang/MaangProblems.Tests/412_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using MaangProblems._1672_MaximumWealth;
+using MaangProblems._412_FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaangProblems.Tests._412_FizzBuzz
+{
+    public class FizzBuzz
+    {
+        [TestCaseSource(nameof(FizzBuzz_Data))]
+        public void FizzBuzz_Test(int n, string[] expected)
+        {
+            var solver = new FizzBuzzProblem();
+            var result = solver.FizzBuzz(n);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        static IEnumerable<TestCaseData> FizzBuzz_Data()
+        {
+            yield return new TestCaseData(
+                3,
+                new[] { "1", "2", "Fizz" }
+            ).SetName("412_LeetCode_FizzBuzz_Test 1");
+
+            yield return new TestCaseData(
+                5,
+                new[] { "1", "2", "Fizz", "4", "Buzz" }
+            ).SetName("412_LeetCode_FizzBuzz_Test 2");
+
+            yield return new TestCaseData(
+                15,
+                new[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" }
+            ).SetName("412_LeetCode_FizzBuzz_Test 3");
+        }
+    }
+}
---
+++ Maang/MaangProblems/412_FizzBuzz/FizzBuzzProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaangProblems._412_FizzBuzz
+{
+    public class FizzBuzzProblem
+    {
+        public string[] FizzBuzz(int n)
+        {
+            string[] result = new string[n];
+            for (int i = 1; i < n+1; i++)
+            {
+                bool div3 = i % 3 == 0;
+                bool div5 = i % 5 == 0;
+                string tempResult = null;
+                if(div3 && div5){
+                    tempResult = "FizzBuzz";
+                }
+                else if (div3)
+                {
+                    tempResult = "Fizz";
+                }
+                else if (div5)
+                {
+                    tempResult = "Buzz";
+                }
+                else
+                {
+                    tempResult = i.ToString();
+                }
+                result[i-1] = tempResult;
+            }
+            return result;
+        }
+    }
+}

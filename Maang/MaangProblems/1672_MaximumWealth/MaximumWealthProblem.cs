56443c522686c547423e6d175e68499c97641a97
Ravishankar Rajappa <rrajappa@explorance.com>
2026-04-12 18:31:54 +0400

    Maximum wealth works

--- Maang/MaangProblems/1672_MaximumWealth/MaximumWealthProblem.cs
+++ Maang/MaangProblems/1672_MaximumWealth/MaximumWealthProblem.cs
@@ -10,7 +10,15 @@ namespace MaangProblems._1672_MaximumWealth
     {
         public int MaximumWealth(int[][] accounts)
         {
-            return -1;
+            int max = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                    sum += accounts[i][j];
+                max = sum > max ? sum : max;
+            }
+            return max;
         }
     }
 }

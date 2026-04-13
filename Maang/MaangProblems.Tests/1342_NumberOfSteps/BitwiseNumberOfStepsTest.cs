1d36fa956d837475440f797106cef303296de432
Ravishankar Rajappa <rrajappa@explorance.com>
2026-04-13 15:06:38 +0400

    Bitwise Number of steps solved

---
+++ Maang/MaangProblems.Tests/1342_NumberOfSteps/BitwiseNumberOfStepsTest.cs
@@ -0,0 +1,19 @@
+﻿using MaangProblems.NumberOfSteps;
+
+namespace MaangProblems.Tests.NumberOfSteps
+{
+    public class BitwiseNumberOfStepsTest
+    {
+        [TestCase(14, 6, TestName = "1342_leetCode_bitwise_NumberOfSteps_Test 1")]
+        [TestCase(8, 4, TestName = "1342_leetCode_bitwise_NumberOfSteps_Test 2")]
+        [TestCase(30, 8, TestName = "1342_leetCode_bitwise_NumberOfSteps_Test 3")]
+        [TestCase(123, 12, TestName = "1342_leetCode_bitwise_NumberOfSteps_Test 4")]
+        public void BitwiseNumberOfSteps_Test(int num, int expected)
+        {
+            var solver = new BitwiseNumberOfStepsProblem();
+            var result = solver.Solve(num);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Maang/MaangProblems/1342_NumberOfSteps/BitwiseNumberOfStepsProblem.cs
@@ -0,0 +1,23 @@
+﻿
+namespace MaangProblems.NumberOfSteps
+{
+    public class BitwiseNumberOfStepsProblem
+    {
+        public int Solve(int num)
+        {
+            int stepCount = 0;
+            while (num != 0)
+            {
+                if ((num & 1) == 0)
+                    num = num >> 1; 
+                    //Halves as we shift bits to right
+                    //Doubles as we shift bits left
+                else
+                    num--;
+                stepCount++;
+            }
+            return stepCount;
+        }
+
+    }
+}

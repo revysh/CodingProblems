6e748b996ccec05c34ca951e0303f7ebd8618358
Ravishankar Rajappa <rrajappa@explorance.com>
2026-04-14 18:27:49 +0400

    Solved max consecutive ones

--- Maang/MaangProblems.Tests/LeetCodeProblems/485_MaxConsecutiveOnes/MaxConsecutiveOnes.cs
+++ Maang/MaangProblems.Tests/LeetCodeProblems/485_MaxConsecutiveOnes/MaxConsecutiveOnes.cs
@@ -1,12 +1,14 @@
 ﻿
 using NUnit.Framework;
-using MaangProblems.MaxConsecutiveOnes;
+using MaangProblems._485_MaxConsecutiveOnes;
 
-namespace MaangProblems.Tests.MaxConsecutiveOnes
+namespace MaangProblems.Tests._485_MaxConsecutiveOnes
 {
     public class MaxConsecutiveOnesTests
     {
         [TestCase(new[] { 1, 1, 0, 1, 1, 1 }, 3, TestName = "Max Consecutive Ones 1")]
+        [TestCase(new[] { 1 }, 1, TestName = "Max Consecutive Ones 2")]
+        [TestCase(new[] { 1, 0 }, 1, TestName = "Max Consecutive Ones 3")]
         public void MaxConsecutiveOnes_Test(int[] nums, int expected)
         {
             var solver = new MaxConsecutiveOnesProblem();
---
+++ Maang/MaangProblems/LeetCodeProblems/485_MaxConsecutiveOnes/MaxConsecutiveOnes.cs
@@ -0,0 +1,31 @@
+﻿
+namespace MaangProblems._485_MaxConsecutiveOnes
+{
+    public class MaxConsecutiveOnesProblem
+    {
+        public int Solve(int[] nums)
+        {
+            int maxLength = 0;
+            int currentMax = 0;
+            foreach (int x in nums)
+            {
+                currentMax = x==1? currentMax + 1:0;
+                maxLength =  currentMax > maxLength ? currentMax : maxLength;
+            }
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] == 1)
+            //    {
+            //        currentMax++;
+            //        if (currentMax > maxLength)
+            //            maxLength = currentMax;
+            //    }
+            //    else
+            //    {
+            //        currentMax = 0;
+            //    }
+            //}
+            return maxLength;
+        }
+    }
+}
